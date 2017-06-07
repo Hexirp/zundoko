224a6701598b27f71d2ee4ef3976322fd245ef3a
Hexirp <Hexirp@users.noreply.github.com>
2017-06-07 10:38:51 +0900

    Define 'toZundokoS'

--- src/Zundoko.hs
+++ src/Zundoko.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Zundoko
  ( zundoko
  , Zundoko(Zun, Doko, Kiyoshi)
  ) where
  import Prelude
+ import Control.Category ((>>>))
 
  zundoko :: [Int] -> Zundoko
  zundoko = runZundokoS . toZundokoS
@@ -13,6 +16,12 @@ module Zundoko
  runZundokoS = undefined
  
  toZundokoS :: [Int] -> ZundokoS
- toZundokoS = undefined
+ toZundokoS = foldr eoz zun
+  where
+   eoz = ZunS $ ZunS $ ZunS $ ZunS dos
+   dos = DokoS dos
+   zun = (`mod` 2) >>> (== 1) >>> \case
+    False -> ZunS
+    True -> DokoS
  
  data ZundokoS = ZunS ZundokoS | DokoS ZundokoS
