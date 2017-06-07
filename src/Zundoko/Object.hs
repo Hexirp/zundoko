463aeb58f2e1bd7c70059444b0bb2e7f89ee11b8
Hexirp <Hexirp@users.noreply.github.com>
2017-06-07 21:19:23 +0900

    Clean 'randGen'

--- src/Zundoko/Object.hs
+++ src/Zundoko/Object.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Zundoko.Object where
  import System.Random
  import Control.Object
+ import Control.Monad.State.Strict
  import Prelude
 
  data Rand ret where
   Next :: (Random a) => Rand a
 
  randGen :: (Monad m, RandomGen a) => a -> Object Rand m
- randGen x = Object obj
-  where
-   obj :: Monad m => Rand a -> m (a, Object Rand m)
-   obj Next = do
-    let (a, g) = random x
-    return $ (a, randGen g)
+ randGen = stateful $ \case
+  Next -> state random
