2a4349209c87d446129dcd4a83cd8145767f1de9
Hexirp <Hexirp@users.noreply.github.com>
2017-06-08 21:28:23 +0900

    Add 'zundokoMtl'

--- src/Zundoko/Object.hs
+++ src/Zundoko/Object.hs
@@ -5,6 +5,7 @@ module Zundoko.Object where
  import System.Random
  import Control.Object
  import Control.Monad.State.Strict
+ import Control.Monad.Trans.Maybe
  import Prelude
 
  randGen :: (RandomGen a, Random r, Monad m) => a -> Object ((->) r) m
@@ -16,3 +17,21 @@ module Zundoko.Object where
   lift $ do
    (n, _) <- obj @- id
    return $ (n `mod` 2) == 1
+ 
+ zundokoMtr :: (Monad m) => Int -> Object ((->) Bool) m -> Object ((->) Bool) (MaybeT m)
+ zundokoMtr = curry $ stateful $ flip fmap $ s
+  where
+   -- :: StateT (Int, Object ((->) Bool) (MaybeT m) Bool
+   s = do
+    (n, obj) <- get
+    (b, obj') <- lift $ lift $ obj @- id
+    case b of
+     False -> put (n + 1, obj')
+     True -> if 4 <= n
+      then
+       lift $ nothing
+      else
+       put (0, obj')
+    return $ b
+   -- :: MaybeT m a
+   nothing = MaybeT $ return Nothing
