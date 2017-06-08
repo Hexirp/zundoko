9eaa99b3f75e006e55c554166826f742e01beaa7
Hexirp <Hexirp@users.noreply.github.com>
2017-06-08 20:51:40 +0900

    Add 'zundokoStr'

--- src/Zundoko/Object.hs
+++ src/Zundoko/Object.hs
@@ -8,4 +8,11 @@ module Zundoko.Object where
  import Prelude
 
  randGen :: (RandomGen a, Random r, Monad m) => a -> Object ((->) r) m
- randGen = stateful $ (<$> state random)
+ randGen = stateful $ flip fmap $ state random
+
+ zundokoStr :: (Monad m) => Object ((->) Int) m -> Object ((->) Bool) m
+ zundokoStr = stateful $ flip fmap $ do
+  obj <- get
+  lift $ do
+   (n, _) <- obj @- id
+   return $ (n `mod` 2) == 1
