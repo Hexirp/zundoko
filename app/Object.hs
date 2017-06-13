a52e7d3928f3651c3568dec89a8bde7011833574
Hexirp <Hexirp@users.noreply.github.com>
2017-06-13 20:29:14 +0900

    Add "Object"

---
+++ app/Object.hs
@@ -0,0 +1,12 @@
+module Object where
+ import System.Random
+ import Zundoko.Object
+ import Prelude
+
+ zundoko :: IO ()
+ zundoko = do
+  g <- getStdGen
+  let fol x = case x of {
+   False -> (putStrLn "Zun" >>);
+   True -> (putStrLn "Doko" >>)}
+  foldStream (putStrLn "Kiyoshi") fol $ zundokoMtr (zundokoStr $ randGen g) 0
