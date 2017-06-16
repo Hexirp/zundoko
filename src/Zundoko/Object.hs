f12d2d11f62507cadf6f638698092583c76a0aa4
Hexirp <Hexirp@users.noreply.github.com>
2017-06-16 14:52:45 +0900

    Clean types

--- src/Zundoko/Object.hs
+++ src/Zundoko/Object.hs
@@ -7,15 +7,15 @@ module Zundoko.Object where
  import Data.Functor.Identity
  import Prelude
 
- randGen :: (RandomGen a, Random r, Monad m) => a -> Object ((->) r) m
+ randGen :: (RandomGen a, Random r, Monad m) => a -> StrObj m r
  randGen = streamObj $ state random
 
- zundokoStr :: (Monad m) => Object ((->) Int) m -> Object ((->) Bool) m
+ zundokoStr :: Monad m => StrObj m Int -> StrObj m Bool
  zundokoStr = streamObj $ do
   n <- await
   return $ (n `mod` 2) == 1
  
- zundokoMtr :: (Monad m) => Object ((->) Bool) m -> Int -> Object ((->) Bool) (MaybeT m)
+ zundokoMtr :: Monad m => StrObj m Bool -> Int -> StrObj (MaybeT m) Bool
  zundokoMtr = streamObj2 $ do
   a <- awaitOn $ lift . lift
   case a of
@@ -31,24 +31,26 @@ module Zundoko.Object where
        lift $ nothing
   return a
 
- foldStream :: r -> (a -> r -> r) -> Object ((->) a) (MaybeT Identity) -> r
+ type StrObj m a = Object ((->) a) m
+
+ foldStream :: r -> (a -> r -> r) -> StrObj (MaybeT Identity) a -> r
  foldStream x f obj = case obj @- id of
   MaybeT (Identity Nothing) -> x
   MaybeT (Identity (Just (a, obj'))) -> f a $ foldStream x f obj'
 
- streamObj :: (Monad m) => StateT s m a -> s -> Object ((->) a) m
+ streamObj :: Monad m => StateT s m a -> s -> StrObj m a
  streamObj s = stateful $ flip fmap s
 
- streamObj2 :: (Monad m) => StateT s1 (StateT s2 m) a -> s1 -> s2 -> Object ((->) a) m
+ streamObj2 :: Monad m => StateT s1 (StateT s2 m) a -> s1 -> s2 -> StrObj m a
  streamObj2 s a b = streamObj s a @>>@ variable b
 
- await :: StateT (Object ((->) a) m) m a
+ await :: StateT (StrObj m a) m a
  await = awaitOn id
 
  awaitOn
-  :: (m (a, Object ((->) a) m) -> n (r, Object ((->) a) m))
-  -> StateT (Object ((->) a) m) n r
+  :: (m (a, StrObj m a) -> n (b, StrObj m a))
+  -> StateT (StrObj m a) n b
  awaitOn f = StateT $ f . (@- id)
 
- nothing :: (Monad m) => MaybeT m a
+ nothing :: Monad m => MaybeT m a
  nothing = MaybeT $ return Nothing
